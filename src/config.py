0077ca41100bfec0e09dafe7029ff00c6fc738ef
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2023-10-15 12:50:36 +0200

    remove ytmusic section from config

--- src/config.py
+++ src/config.py
@@ -35,9 +35,6 @@ class Config:
                 "Esound": {
                     "email": "",
                     "password": ""
-                },
-                "YTMusic": {
-                    "Cookie": ""
                 }
             }
 
